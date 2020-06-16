d2f1d5233fec020e1579387e4f672d8a4b778649
Lukasz Malucha <lucasmalucha@gmail.com>
2020-06-16 15:57:32 +0100

    data

--- htm_to_txt_parser.py
+++ htm_to_txt_parser.py
@@ -19,13 +19,11 @@ FOLDERS = []
 for brand in BRANDS:
     try:
         CURRENT_PATH = Path(Path.cwd(), "EXTRACTED_FILES", brand)
-        BRAND_FOLDERS = [str(x) for x in CURRENT_PATH.iterdir() if x.is_dir()]  
-        for element in BRAND_FOLDERS:      
+        BRAND_FOLDERS = [str(x) for x in CURRENT_PATH.iterdir() if x.is_dir()]
+        for element in BRAND_FOLDERS:
             FOLDERS.append(element)
     except:
         pass
-
-    
 
 
 def sort_strings(l):
@@ -34,24 +32,22 @@ def sort_strings(l):
     for string in l:
         if string[0] not in sorted_strings:
             sorted_strings[string[0]] = []
-        sorted_strings[string[0]].append(string[1]) 
-               
-    
+        sorted_strings[string[0]].append(string[1])
+
     sorted_values = sorted(sorted_strings.items(), key=operator.itemgetter(0))
-    
+
     sorts = []
     for element in sorted_values:
-        sorts.append(list(reversed(element[1]))) 
-        
-        
-    # SIGNAL POSSIBLE TABLE ROW 
-    sorted_string_list = []     
+        sorts.append(list(reversed(element[1])))
+
+        # SIGNAL POSSIBLE TABLE ROW
+    sorted_string_list = []
     for element in sorts:
         " ".join(element)
         row_string = element[0]
         row_string = row_string.replace("\n", " ")
-        sorted_string_list.append(row_string)             
-        
+        sorted_string_list.append(row_string)
+
     return sorted_string_list
 
 
@@ -59,80 +55,70 @@ def sort_strings(l):
 
 def document_pages(soup):
     """Get Page count and page frames in order to sort strings by page"""
-    page_frames = [ data for data in soup.select('div') if "<a name=" in str(data)]    
-    
-    pages_list= []
-    for count,page in enumerate(page_frames):
+    page_frames = [data for data in soup.select('div') if "<a name=" in str(data)]
+
+    pages_list = []
+    for count, page in enumerate(page_frames):
         tup = ()
-        top = int(re.search(r'(?is)(top:)(.*?)(px)',str(page.get('style'))).group(2))
+        top = int(re.search(r'(?is)(top:)(.*?)(px)', str(page.get('style'))).group(2))
         bottom = top + 842
-        tup = (count+1, top, bottom)
+        tup = (count + 1, top, bottom)
         pages_list.append(tup)
-    
-    return pages_list    
-        
-    
+
+    return pages_list
 
 
 def text_per_page(soup, pages):
-    """Return dictionary of {page<n> : "[ Strings per page n ]"} """    
+    """Return dictionary of {page<n> : "[ Strings per page n ]"} """
     pages_list = []
     header_spans = []
-    
-    divs = [ data for data in soup.select('div') if 'top' in str(data)]
-    
+
+    divs = [data for data in soup.select('div') if 'top' in str(data)]
+
     for page in pages:
-        page_text = []  
+        page_text = []
         for div in divs:
-            page_location = re.search(r'(?is)(top:)(.*?)(px)',str(div.get('style'))).group(2)
-            
+            page_location = re.search(r'(?is)(top:)(.*?)(px)', str(div.get('style'))).group(2)
+
             # Get a header area to identify potential title(between 0 - 285px, roughly 33%)            
             if int(page_location) < 285:
-                spans = [ data for data in div.select('span') if 'font-size' in str(data)]
+                spans = [data for data in div.select('span') if 'font-size' in str(data)]
                 for span in spans:
-                    font_size = re.search(r'(?is)(font-size:)(.*?)(px)',str(span.get('style'))).group(2)
+                    font_size = re.search(r'(?is)(font-size:)(.*?)(px)', str(span.get('style'))).group(2)
                     tup = [str(span.text).strip(), int(font_size.strip())]
                     header_spans.append(tup)
-        
+
             # Sort text by page
             if page[1] < int(page_location) < page[2]:
                 page_text.append((int(page_location), str(div.text).strip()))
-                
-                  
+
         page_text_sorted = sort_strings(page_text)
         pages_list.append(page_text_sorted)
-              
-    
-    return pages_list    
+
+    return pages_list
 
 
-
-  
-
-def extract_text(filename):  
+def extract_text(filename):
     input_file_name = Path(filename).stem
     brand = str(filename).split("\\")[-3]
     file_path = Path(Path.cwd(), "EXTRACTED_FILES", brand, input_file_name, filename)
-    
-# Turn html into bs.soup
-    with open(str(file_path), "r", encoding="UTF-8") as f:        
-        soup = bs.BeautifulSoup(f, "lxml")   
+
+    # Turn html into bs.soup
+    with open(str(file_path), "r", encoding="UTF-8") as f:
+        soup = bs.BeautifulSoup(f, "lxml")
         pages = document_pages(soup)
         pages_text = text_per_page(soup, pages)
-  
-# Save data as json
+
+        # Save data as json
         output_txt_path = Path(Path.cwd(), "EXTRACTED_FILES", brand, input_file_name, "pages")
-        output_txt_path.mkdir(parents=True, exist_ok=True) 
+        output_txt_path.mkdir(parents=True, exist_ok=True)
         for i, page in enumerate(pages_text):
             input_file_name = input_file_name.replace(" ", "_")
-            text_file_path = Path(output_txt_path, input_file_name + "-page-" + str(i+1) + ".csv")  
-            df = pd.DataFrame({f"PAGE {i+1}" : page})
+            text_file_path = Path(output_txt_path, input_file_name + "-page-" + str(i + 1) + ".csv")
+            df = pd.DataFrame({f"PAGE {i + 1}": page})
             df.to_csv(text_file_path, encoding='utf-8-sig', index=False)
 
-    
     return pages_text
-
-    
 
 
 def text_extractor():
@@ -142,18 +128,14 @@ def text_extractor():
         htmls = list(Path(folder).glob('*.html'))
         tables = list(Path(folder).glob('*-table-*.csv'))
         tables_folder = Path(folder, "tables")
-        tables_folder.mkdir(parents=True, exist_ok=True)     
-            
+        tables_folder.mkdir(parents=True, exist_ok=True)
+
         for element in tables:
             shutil.copy(str(element), str(tables_folder))
             element.unlink()
-            
-        for element in htmls:
-            extract_text(str(element))    
-            
-            
 
-        
+        for element in htmls:
+            extract_text(str(element))
 
 
 text_extractor()
