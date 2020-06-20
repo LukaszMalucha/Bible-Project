bbdf17fd6200ed5f284bef87da1a6c738010b35e
Lukasz Malucha <lucasmalucha@gmail.com>
2020-06-20 19:10:29 +0100

    data

--- app/bible/settings.py
+++ app/bible/settings.py
@@ -150,11 +150,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+AWS_S3_OBJECT_PARAMETERS = {
+    "Expires": "Fri, 31 Dec 2055 20:00:00 GMT",
+    "CacheControl": "max-age=94608000",
+
+}
+
+AWS_STORAGE_BUCKET_NAME = "forex-app"
+AWS_S3_REGION_NAME = "us-east-1"
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")  ## hidden
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")  ## hidden
+
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_LOCATION = "static"
+STATICFILES_STORAGE = "custom_storages.StaticStorage"
+
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
 
 LOGIN_URL = "/user/login"
 LOGIN_REDIRECT_URL = "/"
